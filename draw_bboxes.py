40187d356d34e95286ff43ad111a76bc1b57bfaf
kot.yuhara <kot.yuhara@trifort.jp>
2019-11-28 17:04:04 +0900

    show confidence

--- draw_bboxes.py
+++ draw_bboxes.py
@@ -23,34 +23,62 @@ class FeatureType(Enum):
     SYMBOL = 5
 
 
-def draw_boxes(image, bounds, color, width=5):
+def draw_boxes(image, bounds, color, confidences=None, width=5):
     draw = ImageDraw.Draw(image)
-    for bound in bounds:
-        draw.line([
-            bound.vertices[0].x, bound.vertices[0].y,
-            bound.vertices[1].x, bound.vertices[1].y,
-            bound.vertices[2].x, bound.vertices[2].y,
-            bound.vertices[3].x, bound.vertices[3].y,
-            bound.vertices[0].x, bound.vertices[0].y], fill=color, width=width)
+    if confidences:
+        for confidence, bound in zip(confidences, bounds):
+            confidence = confidence * 100
+            base_x = bound.vertices[0].x
+            base_y = bound.vertices[0].y - 10
+            if base_x < 0:
+                base_x = 0
+            if base_y < 0:
+                base_y = 0
+            draw.line([
+                bound.vertices[0].x, bound.vertices[0].y,
+                bound.vertices[1].x, bound.vertices[1].y,
+                bound.vertices[2].x, bound.vertices[2].y,
+                bound.vertices[3].x, bound.vertices[3].y,
+                bound.vertices[0].x, bound.vertices[0].y],
+                fill=color, width=width)
+            draw.text((base_x, base_y), "{} %".format(round(confidence)),
+                      (201, 34, 34))
     return image
 
 
-def get_document_bounds(response, feature):
+def get_document_bounds(response, feature, use_confidence=False):
     document = response.full_text_annotation
     bounds = []
-    for i, page in enumerate(document.pages):
+    confidences = []
+    for page in document.pages:
         for block in page.blocks:
             if feature == FeatureType.BLOCK:
                 bounds.append(block.bounding_box)
+                if use_confidence:
+                    confidences.append(block.confidence)
             for paragraph in block.paragraphs:
                 if feature == FeatureType.PARA:
                     bounds.append(paragraph.bounding_box)
+                    if use_confidence:
+                        confidences.append(paragraph.confidence)
                 for word in paragraph.words:
+                    if feature == FeatureType.WORD:
+                        bounds.append(word.bounding_box)
+                        if use_confidence:
+                            confidences.append(word.confidence)
                     for symbol in word.symbols:
                         if feature == FeatureType.SYMBOL:
                             bounds.append(symbol.bounding_box)
-                    if feature == FeatureType.WORD:
-                        bounds.append(word.bounding_box)
+                            if use_confidence:
+                                confidences.append(symbol.confidence)
+    return bounds, confidences
+
+
+def get_bounds(response):
+    # only use text_annotations
+    text_annotations = response.text_annotations
+    bounds = [text_annotation.bounding_poly
+              for text_annotation in text_annotations]
     return bounds
 
 
@@ -78,8 +106,10 @@ def main():
                                  types.AnnotateImageResponse())
 
     image = Image.open(image_file_path)
-    bounds = get_document_bounds(response, feature=ts)
-    new_image = draw_boxes(image, bounds, 'yellow')
+    bounds, confidences = get_document_bounds(response,
+                                              feature=ts, use_confidence=True)
+    # bounds = get_bounds(response)
+    new_image = draw_boxes(image, bounds, 'yellow', confidences)
     new_image.save("result.jpg")
 
 
